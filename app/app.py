b17bb2ce68e9731ffd996c03f91fdad825f1ae36
nickeita <nt89@njit.edu>
2021-07-30 19:40:37 -0400

    Task: included rest api

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+# from typing import List, Dict
+from flask import Flask, render_template, request
+import simplejson as json
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
@@ -24,6 +26,26 @@ def table():
     cursor.execute('SELECT * FROM zillow')
     result = cursor.fetchall()
     return render_template('table.html', houses=result)
+
+
+# @app.route('/search', methods=['POST', 'GET'])
+# def search():
+#     cursor = mysql.get_db().cursor()
+#     input_data = (request.form.get('square_feet'), request.form.get('min_price'), request.form.get('max_price'))
+#     sql_query = """SELECT * FROM zillow WHERE living_space_sqft >= %s AND list_price >= %s AND list_price <= %s"""
+#     cursor.execute(sql_query, input_data)
+#     result = cursor.fetchall()
+#     return render_template('search.html', houses=result)
+
+
+@app.route('/api/v1/houses', methods=['GET'])
+def api_zillow() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM zillow')
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
 
 
 if __name__ == '__main__':
